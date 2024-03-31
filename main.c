0ecff7f5fb07bc6955ff2b25c682f2183da12610
BanzaiNamco <orrin_landon_uy@dlsu.edu.ph>
2024-03-31 19:38:49 +0800

    moved optimization on C kernel to dotprod function

--- main.c
+++ main.c
@@ -6,11 +6,13 @@ extern void x8664(int n, float* arr1, float* arr2, float* sdot);
 
 void dotprod(int n, float* arr1, float* arr2, float* sdot) {
     int i;
-    *sdot = 0.0;
+    float ans = 0.0;
 
     for (i = 0; i < n; i++) {
-        *sdot = *sdot + (arr1[i] * arr2[i]);
+        ans = ans + (arr1[i] * arr2[i]);
     }
+
+    *sdot = ans;
 }
 
 int main() {
@@ -36,8 +38,6 @@ int main() {
     int loop = 30;
     int errCount = 0;
 
-    float ans;
-
     array1 = (float*)malloc(N * sizeof(float));
     array2 = (float*)malloc(N * sizeof(float));
     sdot1 = (float*)malloc(loop * sizeof(float));
@@ -62,8 +62,7 @@ int main() {
 
     begin = clock();
     for (int j = 0; j < loop; j++) {
-        dotprod(N, array1, array2, &ans);
-        sdot1[j] = ans;
+        dotprod(N, array1, array2, &sdot1[j]);
         printf("Dot Product is %.10f\n", sdot1[j]);
     }
     end = clock();
@@ -92,19 +91,38 @@ int main() {
     asm_time[1] = time_taken;
 
     //correctness checking
+    printf("\n---------- Correctness Checking ----------\n");
+    for (int j = 0; j < loop-1; j++) {
+        if (sdot1[j] != sdot1[j+1]) {
+			printf("Error: C function has multiple results\n");
+			printf("Result 1: %.10f\n", sdot1[j]);
+			printf("Result 2: %.10f\n", sdot1[j+1]);
+			errCount++;
+        };
+    }
+
+    if (errCount == 0) {
+        printf("C function exhibits no errors\n");
+    };
+
     for (int j = 0; j < loop; j++) {
         if (sdot1[j] != sdot2[j]) {
-            printf("\nError: C and Assembly function results are not same\n");
+            printf("Error: C and Assembly function results are not same\n");
             printf("C: %.10f\n", sdot1[j]);
             printf("Assembly: %.10f\n", sdot2[j]);
             errCount++;
-            printf("%d\n", j);
         }
     }
-    printf("\nError Count: %d\n", errCount);
+    if (errCount == 0) {
+		printf("Assembly function exhibits no errors\n");
+    }
+    else {
+        printf("Errors found: %d\n", errCount);
+    }
 
 
-    printf("\n\nC function takes %lf milliseconds for array size %d\n", c_time[1], N);
+    printf("\n-------------Time Comparison-------------\n\n");
+    printf("C function takes %lf milliseconds for array size %d\n", c_time[1], N);
     printf("Average Execution time: %lf with loop count: %d\n", c_time[0], loop);
     printf("\n");
 
@@ -112,8 +130,8 @@ int main() {
     printf("Average Execution time: %lf with loop count: %d\n\n", asm_time[0], loop);
 
     //compare both times
-    printf("C function takes %lfx ms more than Assembly\n\n", c_time[0] / asm_time[0]);
-    printf("ASM function takes %lfx ms more than C\n", asm_time[0] / c_time[0]);
+    printf("C function is %lfx slower than Assembly\n\n", c_time[0] / asm_time[0]);
+    printf("ASM function is %lfx slower than C\n", asm_time[0] / c_time[0]);
 
     free(array1);
     free(array2);
