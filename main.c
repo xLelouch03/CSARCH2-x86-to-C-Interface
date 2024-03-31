6cae6d8a94dee7a8c0512cb77c7595af11f556e4
BanzaiNamco <orrin_landon_uy@dlsu.edu.ph>
2024-03-31 18:20:29 +0800

    move prints around for easier reading

--- main.c
+++ main.c
@@ -14,10 +14,13 @@ void dotprod(int n, float* arr1, float* arr2, float* sdot) {
 }
 
 int main() {
-
-    const int N = 1 << 24;
-    const int ARRAY_BYTES = N * sizeof(int);
+    printf("\nArray size: 2^");
+    int input;
+    scanf_s("%d", &input);
+    const int N = 1 << input;
     printf("Array size: %d\n", N);
+    const int ARRAY_BYTES = N * sizeof(int);
+    
 
     //timer variable
     clock_t begin, end;
@@ -28,8 +31,8 @@ int main() {
     float* array2;
     float sdot1;
     float sdot2;
-    double c_time;
-    double asm_time;
+    double c_time[2];
+    double asm_time[2];
     array1 = (float*)malloc(N * sizeof(float));
     array2 = (float*)malloc(N * sizeof(float));
 
@@ -45,10 +48,12 @@ int main() {
         array2[i] = 0.0000003455f;
     }
 
+    printf("---------- C Function ----------\n\n");
     // fill the cache
     dotprod(N, array1, array2, &sdot1);
 
     int loop = 30;
+
     begin = clock();
     for (int j = 0; j < loop; j++) {
         dotprod(N, array1, array2, &sdot1);
@@ -59,14 +64,13 @@ int main() {
     time_taken = ((double)(end - begin)) * 1e3 / CLOCKS_PER_SEC;
     avg_time = time_taken / loop;
 
-    c_time = avg_time;
+    c_time[0] = avg_time;
+    c_time[1] = time_taken;
 
-    printf("C function takes %lf milliseconds for array size %d\n", time_taken, N);
-    printf("Average Execution time: %lf with loop count: %d\n", avg_time, loop);
-    printf("\n");
-
+    printf("\n\n---------- Assembly Function ----------\n\n");
     //fill the cache
     x8664(N, array1, array2, &sdot2);
+
     begin = clock();
     for (int j = 0; j < loop; j++) {
        x8664(N, array1, array2, &sdot2);
@@ -77,14 +81,19 @@ int main() {
     time_taken = ((double)(end - begin)) * 1e3 / CLOCKS_PER_SEC;
     avg_time = time_taken / loop;
 
-    asm_time = avg_time;
+    asm_time[0] = avg_time;
+    asm_time[1] = time_taken;
 
-    printf("Assembly function takes %lf milliseconds for array size %d\n", time_taken, N);
-    printf("Average Execution time: %lf with loop count: %d\n\n", avg_time, loop);
+    printf("\n\nC function takes %lf milliseconds for array size %d\n", c_time[1], N);
+    printf("Average Execution time: %lf with loop count: %d\n", c_time[0], loop);
+    printf("\n");
+
+    printf("Assembly function takes %lf milliseconds for array size %d\n", asm_time[1], N);
+    printf("Average Execution time: %lf with loop count: %d\n\n", asm_time[0], loop);
 
     //compare both times
-    printf("C function takes %lfx ms more than Assembly\n\n", c_time / asm_time);
-    printf("ASM function takes %lfx ms more than C\n", asm_time / c_time);
+    printf("C function takes %lfx ms more than Assembly\n\n", c_time[0] / asm_time[0]);
+    printf("ASM function takes %lfx ms more than C\n", asm_time[0] / c_time[0]);
 
     free(array1);
     free(array2);
